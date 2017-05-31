000dec137a8e6e87b64fd0edd85f822ada78d44e
AskanHolaAE17 <be.in.pair@gmail.com>
2017-05-31 18:19:25 +0300

    02

--- app/mailers/order_non_sector_mailer.rb
+++ app/mailers/order_non_sector_mailer.rb
@@ -11,11 +11,7 @@ class OrderNonSectorMailer < ApplicationMailer
     @root_path = (Constant.find_by title: 'root_path').value
     @human_feedback_form_link = @root_path + 'order_non_sector/feedback_form/' + @user.id.to_s + '/' + @order.akey[0..1] + '/' + @order.id.to_s + '/' + @user.akey[0..1]
     
-<<<<<<< HEAD
-    mail(to: 'vitalina.liane@gmail.com', subject: 'ЛИСТ АДМІНУ: Замовлення консультаії зі спеціалістом')    
-=======
     mail(to: 'vitalina.liane@gmail.com', subject: 'ЛИСТ ДЛЯ АДМІНА: Замовлення консультаії зі спеціалістом')    
->>>>>>> ab51f6212ecb17b335859493472a4e6793637b3b
   end
 
 
